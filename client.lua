51c01d517a70d786605f5b96e1740fb63f76b3cb
MarmotaGit <jaimeadanielfilho@hotmail.com>
2018-11-01 22:13:38 -0300

    Hide on pause.

--- client.lua
+++ client.lua
@@ -6,6 +6,7 @@ Citizen.CreateThread(function()
         Citizen.Wait(100)
         TriggerServerEvent("getBasics")
         SendNUIMessage({
+            show = IsPauseMenuActive(),
             armor = GetPedArmour(GetPlayerPed(-1)),
             health = (GetEntityHealth(GetPlayerPed(-1))-100),
             hunger = hunger,
